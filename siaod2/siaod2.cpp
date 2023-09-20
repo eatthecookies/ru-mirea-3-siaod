25517ccefb24913d15125d02bf9aac21969300c4
eatthecookies <79151974+eatthecookies@users.noreply.github.com>
2023-09-20 10:48:00 +0300

    Добавьте файлы проекта.

---
+++ siaod2/siaod2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int inp;
+    cout << "Меню." << endl;
+    cout << "1. " << endl;
+    cout << "2. " << endl;
+    cout << "3. " << endl;
+    cout << "4. " << endl;
+    cout << "5. " << endl;
+    cin >> inp;
+
+    switch (inp)
+    {
+    case 1:
+        
+    case 2:
+
+    case 3:
+
+    case 4:
+
+    case 5:
+
+    };
+       
+}
